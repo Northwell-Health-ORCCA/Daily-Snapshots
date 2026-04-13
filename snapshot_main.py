e114c9fce41ce30c66f75cf5eae84eb5f66cf26d
David <denglish14@gmail.com>
2026-04-13 11:56:10 -0400

    use json to load data. stored sharedrive

--- snapshot_main.py
+++ snapshot_main.py
@@ -1,9 +1,11 @@
-from mappings import mappings_dict
+# from mappings import mappings_dict
 from snapshot_logger import logger
 from snapshot_updated import *
 from datetime import date as dt
+import json
 
 if __name__ == '__main__':
+    DIRECTORY = r"//NT2KWB972SRV03/SHAREDATA/CPP-Data/Sutherland RPA/Northwell Process Automation ETM Files/Monthly Reports/Daily Snapshot" # r"M:/CPP-Data/Sutherland RPA/Northwell Process Automation ETM Files/Monthly Reports"
     today = dt.today()
     dt_func = DateFunctions()
     # # date = dt_func.ask_if_correct_date(today)
@@ -13,11 +15,15 @@ if __name__ == '__main__':
         outbound_df = MainSpreadsheet()
         outbound_df.run(date)
     except FileNotFoundError:
-        logger.critical(f"Main outbound spreadsheet not found in M:\CPP-Data\Sutherland RPA\Combined Outputs")
+        logger.critical(f"Main outbound spreadsheet not found in M:/CPP-Data/Sutherland RPA/Combined Outputs")
     else:
+        #open mappings.json from the DIRECTORY
+        with open(f"{DIRECTORY}//mappings.json", "r") as f:
+                mappings_dict = json.load(f)
         for use_case in mappings_dict:
-        # for use_case in ['BD IS Printing']:
-            snapshot = Snapshot(use_case, outbound_df.main_df, outbound_df.file_date)
+        # for use_case in ['BD IS Printing']: #used for ad hoc use case snapshot
+            
+            snapshot = Snapshot(use_case, outbound_df.main_df, outbound_df.file_date, mappings_dict)
             try:
                 snapshot.parse_spreadsheet()
                 if len(snapshot.use_case_df) == 0:
