7bd3ff5ab74d84a3b8a9cc9561f9ffa6d61490dd
Danielle Pashayan <87950814+DaniellePashayan@users.noreply.github.com>
2026-05-21 08:47:17 -0400

    converted to use UV

--- Bot Execution Report/ber.bat
+++ Bot Execution Report/ber.bat
@@ -3,11 +3,11 @@
 SETLOCAL
 set FILE_PATH=%~dp0
 set SCRIPT_PATH=%FILE_PATH%cs_ber.py
-python -u "%SCRIPT_PATH%"
+uv run "%SCRIPT_PATH%"
 set SCRIPT_PATH=%FILE_PATH%typeb_ber.py
-python -u "%SCRIPT_PATH%"
+uv run "%SCRIPT_PATH%"
 set SCRIPT_PATH=%FILE_PATH%lij_cbo_ber_email.py
-python -u "%SCRIPT_PATH%"
+uv run "%SCRIPT_PATH%"
 ENDLOCAL
 
 @Echo Process Completed.
--- snapshot.bat
+++ snapshot.bat
@@ -3,5 +3,5 @@
 SETLOCAL
 set FILE_PATH=%~dp0
 set SCRIPT_PATH=%FILE_PATH%snapshot_main.py
-python -u "%SCRIPT_PATH%"
+uv run "%SCRIPT_PATH%"
 ENDLOCAL
